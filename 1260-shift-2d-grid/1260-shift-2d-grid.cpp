03758f007b79e565fe703385e61d3caf433c58ea
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-04-12 00:09:07 +0530

    Updated some comments

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> ans(m, vector<int>(n,0));
+        int m=grid.size(); //No of rows
+        int n=grid[0].size(); //No of columns
+        vector<vector<int>> ans(m, vector<int>(n,0)); //vector<int> (n, 0)" means a vector having "m" elements each of value "0"
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int y=(j+k)%n;
