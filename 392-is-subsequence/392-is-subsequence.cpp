8b1f1349bde94f4073179e5624aa438e521da9b9
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-04-29 23:25:51 +0530

    Time: 5 ms (23.94%), Space: 6.4 MB (68.53%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        if(s==("")){
+            return true;
+        }
+        
+        int index =0;
+        for(int i=0;i<t.length();i++){
+            if(t[i]==s[index]){
+                index++;
+            }
+            if(index>=s.length()){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
