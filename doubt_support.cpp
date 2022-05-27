3d741064ab71bf7375f331a51e7108ef4dc1ecb9
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-05-27 15:02:35 +0530

    doubt support

---
+++ doubt_support.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+ int n;
+ cin>>n;
+ if (n<=1600){
+     cout<<"yes"<<endl;
+ }
+ else {
+     cout<<"no"<<endl;
+ }
+ return 0;
+}
