541f8df24e0f8ba33111de8e4bd53b2ee54ba9bc
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-06-28 14:13:38 +0530

    Time: 25 ms (70.23%), Space: 17 MB (22.78%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum^=nums[i];
+        }
+        return sum;
+    }
+};
