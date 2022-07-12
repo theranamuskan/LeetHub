ea70edf06b46c66b91c9849d18ecb4dadd57b86b
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-07-13 00:05:50 +0530

    Time: 142 ms (52.19%), Space: 46.6 MB (78.97%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        return unique(nums.begin(),nums.end())!=nums.end();
+        return unique(nums.begin(),nums.end())!=nums.end();  
     }
 };
