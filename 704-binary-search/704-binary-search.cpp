455da4a8f85eb8bab99496acbc69aca4badf2fe8
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-07-19 22:33:43 +0530

    Time: 58 ms (48.37%), Space: 27.6 MB (12.32%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -7,7 +7,7 @@ public:
         int right = nums.size() - 1;
         while (left <= right)
         {
-            int mid = (left + right) / 2;
+            int mid = left + (right-left) / 2;
             if (nums[mid] == target)
                 return mid;
             if (nums[mid] < target)
