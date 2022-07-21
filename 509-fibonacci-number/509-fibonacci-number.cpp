3dcfd07f2af2adb39ea354033376535da4ea0757
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-07-21 11:59:31 +0530

    Time: 3 ms (58.55%), Space: 5.8 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,25 @@
+// class Solution
+// {
+// public:
+//     int fib(int n)
+//     {
+//         if (n <= 1)
+//             return n;
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// };
 class Solution
 {
 public:
     int fib(int n)
     {
-        if (n <= 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
+        int fib[n + 2]; //declare an array to store fibonacci number
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
     }
 };
