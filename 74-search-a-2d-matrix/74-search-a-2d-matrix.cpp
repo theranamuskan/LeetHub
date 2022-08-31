35c65ed7aea9e8e5c5fd682e318e7cde5bf801e2
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-08-31 20:36:29 +0530

    Time: 11 ms (20.96%), Space: 9.5 MB (51.41%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,55 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+        
+// 		//checking some base bases
+//         if(n == 0 || m == 0)
+//             return false;
+        
+// 		// treating matrix as array just taking care of indices
+//         int start = 0, end = m*n - 1;
+        
+//         while(start <= end)
+//         {
+//             int mid = start + (end - start) / 2;
+// 			// a[x] : matrix[x / m][x % m] formulae
+//             int ind = matrix[mid/m][mid%m];
+//             if (target == ind)
+//                 return true;
+// 			// left half
+//             else if(target < ind)
+//                 end = mid - 1;
+//             else
+// 			// right half
+//                 start = mid + 1;       
+//         }
+//         return false;
+//     }
+// };
+
+
+
+ // OR
+
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size(),
+			cols = matrix[0].size(),
+            row = 0, col = cols - 1;
+			
+        while (row < rows && col > -1) {
+            int cur = matrix[row][col];
+            if (cur == target) return true;
+            if (target > cur) row++;
+            else col--;
+        }
+        
+        return false;
+    }
+};
