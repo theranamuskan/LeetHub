ee4be640efd8b1a884eda04f2329a594df946f55
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-10-28 22:54:03 +0530

    Time: 35 ms (95.91%), Space: 19.7 MB (77.24%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> M;
+        for(int  i = 0; i < strs.size(); i++)
+        {
+            string str = strs[i];
+            sort(strs[i].begin(), strs[i].end()); // Sorting the string
+            M[strs[i]].push_back(str);  // Sorted string is the key and the value is the initial string
+        }
+        for(auto i = M.begin(); i != M.end(); i++)
+            ans.push_back(i -> second);  // Traversing the map and adding the vectors of string to ans
+        return ans;
+    }
+};
